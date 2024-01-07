3504b2eccd937e28847850602ccd9a8877986c0f
Yevhenii Lysenko <wanderman.man.man@gmail.com>
2024-01-07 18:50:19 +0200

    Createad Cell class and interface

---
+++ ProcessItSkeletonImplementations/Cell.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcessItSkeletons
+{
+    public class Cell
+    {
+
+
+    }
+}
---
+++ ProcessItSkeletons/Implementations/Cell.cs
@@ -0,0 +1,15 @@
+﻿using ProcessItSkeletons.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessItSkeletons.Implementations
+{
+    public class Cell : ICell
+    {
+
+        public Cell() { }
+    }
+}
